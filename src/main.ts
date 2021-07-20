e954adada8d0b9151673de98099b50e5e47f3b51
tomq <512309244@qq.com>
2021-07-20 22:31:08 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as fs from 'fs';
+// import * as fs from 'fs';
 
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: fs.readFileSync('../secrets/private_key.pem'),
-    cert: fs.readFileSync('../secrets/rsa_public_key.pem'),
-  };
+  // const httpsOptions = {
+  //   key: fs.readFileSync('../secrets/private.key'),
+  //   cert: fs.readFileSync('../secrets/cert.pem'),
+  // };
   // const app = await NestFactory.create<NestFastifyApplication>(
   //   AppModule,
   //   new FastifyAdapter({ https: httpsOptions }),
   // );
 
-  const app = await NestFactory.create(AppModule, { httpsOptions });
+  // const app = await NestFactory.create(AppModule, { httpsOptions });
+  const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
 bootstrap();
